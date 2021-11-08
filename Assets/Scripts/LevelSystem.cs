3033bb453b0493213c8acf48f684eda7cc862cc1
Dennis Fransson <dennis.fransson00@hotmail.se>
2021-11-08 14:32:22 +0100

    Small Fixing

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Events;
 
 public class LevelSystem : MonoBehaviour
 {
+    public UnityEvent<int> LevelUpEvent;
+    public UnityEvent<float, float> UpdateXpEvent;
     public int level;
     public float currentXp;
     public float requiredXp;
 
-    private float lerpTimer;
-    private float delayTimer;
-    [Header("UI")]
-    public Image frontXpBar;
-    public Image backXpBar;
-    public TextMeshProUGUI levelText;
-    public TextMeshProUGUI xpText;
+    //[Header("UI")]
+    //public Image frontXpBar;
+    //public Image backXpBar;
+    //public TextMeshProUGUI levelText;
+    //public TextMeshProUGUI xpText;
     [Header("Multipliers")]
     [Range(1f, 300f)]
     public float additionMultiplier = 300;
@@ -27,45 +28,29 @@ public class LevelSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        frontXpBar.fillAmount = currentXp / requiredXp;
-        backXpBar.fillAmount = currentXp / requiredXp;
+        // frontXpBar.fillAmount = currentXp / requiredXp;
+        // backXpBar.fillAmount = currentXp / requiredXp;
+        level = 1;
         requiredXp = CalculateRequiredXp();
-        levelText.text = "Level " + level;
+        UpdateXpEvent.Invoke(currentXp, requiredXp);
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateXpUI();
-        if (Input.GetKeyDown(KeyCode.Equals))
+        // UpdateXpUI();
+        if (Input.GetKeyDown(KeyCode.X))
             GainExperienceFlatRate(20);
         if (currentXp > requiredXp)
             LevelUp();
     }
 
-    public void UpdateXpUI()
-    {
-        float xpFraction = currentXp / requiredXp;
-        float FXP = frontXpBar.fillAmount;
-        if (FXP < xpFraction)
-        {
-            delayTimer += Time.deltaTime;
-            backXpBar.fillAmount = xpFraction;
-            if (delayTimer > 0.3)
-            {
-                lerpTimer += Time.deltaTime;
-                float percentComplete = lerpTimer / 4;
-                frontXpBar.fillAmount = Mathf.Lerp(FXP, backXpBar.fillAmount, percentComplete);
-            }
-        }
-        xpText.text = currentXp + "/" + requiredXp;
-    }
-
     public void GainExperienceFlatRate(float xpGained)
     {
         currentXp += xpGained;
-        lerpTimer = 0f;
-        delayTimer = 0f;
+        UpdateXpEvent.Invoke(currentXp, requiredXp);
+        // lerpTimer = 0f;
+        // delayTimer = 0f;
     }
 
     public void GainExperienceScalable(float xpGained, int passedLevel)
@@ -79,19 +64,21 @@ public class LevelSystem : MonoBehaviour
         {
             currentXp += xpGained;
         }
-        lerpTimer = 0f;
-        delayTimer = 0f;
+        // lerpTimer = 0f;
+        // delayTimer = 0f;
     }
 
     public void LevelUp()
     {
         level++;
-        frontXpBar.fillAmount = 0f;
-        backXpBar.fillAmount = 0f;
+        // frontXpBar.fillAmount = 0f;
+        // backXpBar.fillAmount = 0f;
         currentXp = Mathf.RoundToInt(currentXp - requiredXp);
-        GetComponent<PlayerHealth>().IncreaseHealth(level);
+        // GetComponent<PlayerHealth>().IncreaseHealth(level);
         requiredXp = CalculateRequiredXp();
-        levelText.text = "Level " + level;
+        // levelText.text = "Lvl " + level;
+        LevelUpEvent.Invoke(level);
+        UpdateXpEvent.Invoke(currentXp, requiredXp);
     }
 
     private int CalculateRequiredXp()
--- Assets/UI Scripts/BattleHUD.cs
+++ Assets/UI Scripts/BattleHUD.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BattleHUD : MonoBehaviour
 {
-    public Text nameText;
-    public Text levelText;
-    public Text healthText;
+
+    public TMP_Text nameText;
+    public TMP_Text levelText;
+    public TMP_Text healthText;
     public Slider healthSlider;
+
+    public Image frontXpBar;
+    public Image backXpBar;
+    public TextMeshProUGUI xpText;
+
+    private float lerpTimer;
+    private float delayTimer;
 
     public void SetHUD(Unit unit)
     {
         nameText.text = unit.unitName;
-        levelText.text = "Lvl" + unit.unitLevel;
+        levelText.text = "Lvl " + unit.unitLevel;
         // healthText.text = unit.currentHealth;
         healthSlider.maxValue = unit.maxHealth;
         healthSlider.value = unit.currentHealth;
@@ -22,5 +31,40 @@ public class BattleHUD : MonoBehaviour
     public void SetHealth(int health)
     {
         healthSlider.value = health;
+    }
+
+    public void SetLevel(int level)
+    {
+        levelText.text = "Lvl " + level.ToString();
+    }
+
+    public void UpdateXpUI(float currentXp, float requiredXp)
+    {
+        float xpFraction = currentXp / requiredXp;
+        float FXP = frontXpBar.fillAmount;
+        frontXpBar.fillAmount = currentXp / requiredXp;
+        backXpBar.fillAmount = currentXp / requiredXp;
+        if (FXP < xpFraction)
+        {
+            StartCoroutine(AnimateXpBar(currentXp, requiredXp));
+        }
+        xpText.text = currentXp + "/" + requiredXp;
+    }
+
+    private IEnumerator AnimateXpBar(float currentXp, float requiredXp)
+    {
+
+        float xpFraction = currentXp / requiredXp;
+        float FXP = frontXpBar.fillAmount;
+        delayTimer += Time.deltaTime;
+        backXpBar.fillAmount = xpFraction;
+        if (delayTimer > 0.3)
+        {
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / 4;
+            frontXpBar.fillAmount = Mathf.Lerp(FXP, backXpBar.fillAmount, percentComplete);
+            
+            yield return null;
+        }
     }
 }
